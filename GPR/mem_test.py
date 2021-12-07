c31fd6b755c391a813f7083528602bd3a3983b77
Lorena Magaña Zertuche <lmzertuche@t3420n-1240al.ipamnet.ipam.ucla.edu>
2021-12-06 16:07:17 -0800

    Adding memory test

--- GPR/mem_test.py
+++ GPR/mem_test.py
@@ -1,28 +1,26 @@
-import sys
 import csv
 import numpy as np
 
 import sklearn
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn import preprocessing
-
-from plotting_tools import *
-sys.path.insert(0, '/home/IPAMNET/lmzertuche/Documents/LIGO_ML/IPAM2021_ML')
-from read_data import *
-
-
 from sklearn.gaussian_process.kernels import DotProduct, RationalQuadratic, RBF, ConstantKernel, WhiteKernel
 
-xtrain = extractData('../NewRealistic/v0c0train_x.csv')
-ytrain = extractData('../NewRealistic/v0c0train_y.csv')
-xtest = extractData('../NewRealistic/v0c0test_x.csv')
-ytest = extractData('../NewRealistic/v0c0test_y.csv')
+from read_data import *
 
+# Read data from files
+xtrain = extractData('v0c0train_x.csv')
+ytrain = extractData('v0c0train_y.csv')
+xtest = extractData('v0c0test_x.csv')
+ytest = extractData('v0c0test_y.csv')
+
+# Remove the last column (M_chirp) of the dataset
 xtrain = xtrain[:,:-1]
 ytrain = ytrain[:,:-1]
 xtest = xtest[:,:-1]
 ytest = ytest[:,:-1]
 
+# Standardize the data
 scaler = preprocessing.StandardScaler().fit(xtrain)
 xtrain_scaled = scaler.transform(xtrain)
 scaler = preprocessing.StandardScaler().fit(xtest)
@@ -32,8 +30,18 @@ ytrain_scaled = scaler.transform(ytrain)
 scaler = preprocessing.StandardScaler().fit(ytest)
 ytest_scaled = scaler.transform(ytest)
 
+##Only take part of the data to train and test
+#xtrain_scaled = xtrain_scaled[:10000,:]
+#ytrain_scaled = ytrain_scaled[:10000,:]
+#xtest_scaled = xtest_scaled[:2000,:]
+#ytest_scaled = ytest_scaled[:2000,:]
+
+# Train GPR and predict data
 kernel = DotProduct() + RationalQuadratic(length_scale=1.0, alpha=2, length_scale_bounds=(.1, 10), alpha_bounds=(1e-4, 4))
-gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-7, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=0, copy_X_train=False)
+gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-7, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=3, copy_X_train=False)
 gpr.fit(xtrain_scaled, ytrain_scaled)
 predicted_data, std = gpr.predict(xtest_scaled, return_std=True)
-gpr.score(xtest_scaled, ytest_scaled)
+print('The score is: ', gpr.score(xtest_scaled, ytest_scaled))
+
+# Write results in csv file
+writeResult('GPR_DP+RQ_nochirp_v0c0.csv',predicted_data)
